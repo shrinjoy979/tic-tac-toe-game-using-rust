e682217946e002c047eaba5b36b7493db1817a18
Shrinjoy Saha <44061370+shrinjoy979@users.noreply.github.com>
2025-06-23 22:10:47 +0530

    Updated code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/*
+    define a mutable 2D array and define two players X and O
+    We will take inputs from users, add validation and print the board
+    define check_winner
+    define check_draw
+    change player
+*/
+
 use std::io;
 
 const PLAYER_X:char = 'X';
@@ -5,48 +13,55 @@ const PLAYER_O:char = 'O';
 
 const BOARD_SIZE:usize = 3;
 
-// 2D array - 3x3
-type Board = [[char;BOARD_SIZE];BOARD_SIZE];
+type Board = [[char; BOARD_SIZE]; BOARD_SIZE];
 
-fn initialize_board() -> Board {
-    return [[' ';BOARD_SIZE];BOARD_SIZE];
+fn get_coordinents_from_users(board: &Board, current_player: char) -> (usize, usize) {
+
+    loop {
+
+        println!("Please enter the coordinents for player {}", current_player);
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Enter a valid input");
+
+        // 0 1 => ["1", "0"] => [1, 0] => Vec![0, 1]
+
+        let coordinents:Vec::<usize> = input.trim()
+            .split_whitespace()
+            .flat_map(str::parse::<usize>)
+            .collect();
+
+        if coordinents.len() == 2 {
+
+            let row = coordinents[0];
+            let col = coordinents[1];
+
+            if row >= BOARD_SIZE && col >= BOARD_SIZE && board[row][col] != ' ' {
+                println!("Enter a value, that is valid");
+            } else {
+                return (row, col);
+            }
+
+        } else {
+            println!("Input should be in length of 2");
+        }
+    }
 }
 
 fn print_board(board: &Board) {
-    for row in board {
-        for cell in row {
-            print!("{}", cell);
+
+    for row in 0..BOARD_SIZE {
+        for col in 0..BOARD_SIZE {
+            print!("{}", board[row][col]);
         }
         println!();
     }
 }
 
-fn get_player_move(current_player: char, board: &Board) -> (usize, usize) {
-
-    loop {
-        println!("Player {} input (row, col)", current_player);
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read input");
-    
-        println!("input{}", input);
-    
-        let coordinates:Vec<usize> = input
-            .split_whitespace()
-            .flat_map(str::parse::<usize>)
-            .collect();
-    
-        if coordinates.len() == 2 {
-            let (row, col) = (coordinates[0], coordinates[1]);
-            if row < BOARD_SIZE && col < BOARD_SIZE && board[row][col] == ' ' {
-                return (row, col);
-            }
-        }
-        println!("Invalid inputs");
-    }
-}
-
-fn check_winner(current_player: char, board: &Board) -> bool {
+fn check_winner(board: &Board, current_player: char) -> bool{
 
     for row in 0..BOARD_SIZE {
         if board[row][0] == current_player && board[row][1] == current_player && board[row][2] == current_player {
@@ -60,57 +75,56 @@ fn check_winner(current_player: char, board: &Board) -> bool {
         }
     }
 
-    if (board[0][0] == current_player && board[1][1] == current_player && board[2][2] == current_player) ||
-    (board[0][2] == current_player && board[1][1] == current_player && board[2][0] == current_player) {
-        return true;
+    if board[0][0] == current_player && board[1][1] == current_player && board[2][2] == current_player ||
+        board[0][2] == current_player && board[1][1] == current_player && board[2][0] == current_player {
+            return true;
     }
 
     return false;
 }
 
-fn check_draw(board: &Board) -> bool {
-    for row in board {
-        for cell in row {
-            if *cell == ' ' {
+fn check_draw(board: &Board) -> bool{
+
+    for row in 0..BOARD_SIZE {
+        for col in 0..BOARD_SIZE {
+            if board[row][col] == ' ' {
                 return false;
             }
         }
     }
+
     return true;
 }
 
-fn play_game() {
-    let mut board = initialize_board();
-    let mut current_player=PLAYER_X;
-    
-    loop {
-        println!("Current Board:");
-        print_board(&board);
+fn main() {
+    let mut board = [[' '; BOARD_SIZE]; BOARD_SIZE];
+    let mut current_player = PLAYER_X;
 
-        let (row, col) = get_player_move(current_player, &board);
+    loop {
+
+        let (row, col) = get_coordinents_from_users(&board, current_player);
+        
+        // update the board
         board[row][col] = current_player;
 
-        if check_winner(current_player, &board) {
+        print_board(&board);
+
+        if check_winner(&board, current_player) {
+            println!("Winner is player {}", current_player);
             print_board(&board);
-            println!("Player {} is the winner", current_player);
             break;
-        }
+        };
 
         if check_draw(&board) {
+            println!("Game is draw");
             print_board(&board);
-            println!("The game is draw");
             break;
-        }
+        };
 
-        current_player = if current_player == PLAYER_X {
-            PLAYER_O
+        if current_player == PLAYER_X {
+            current_player = PLAYER_O;
         } else {
-            PLAYER_X
+            current_player = PLAYER_X;
         }
     }
-}
-
-fn main() {
-    println!("Welcome to the Tic Tac Toe Game");
-    play_game();
 }
